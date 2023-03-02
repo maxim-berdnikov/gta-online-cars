2b1d5dc4bce2c4655ad4bd2ce00b3f3a61f80eb5
Maxim Berdnikov <36860922+maxim-berdnikov@users.noreply.github.com>
2023-03-02 09:43:55 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.scss";
+import { Header } from "./templates/components";
+
+export const App = (): JSX.Element => {
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
+};
